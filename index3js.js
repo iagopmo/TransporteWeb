329d338ed67b588361ffa559a384b12937347b32
iagopmo <iagopmo@gmail.com>
2020-10-31 18:21:34 -0300

    Add save solicitation function and lastId

---
+++ index3js.js
@@ -0,0 +1,36 @@
+'use strict';
+
+$(document).ready(function () {
+  if (lastId == null) {
+    lastId = 0;
+  } else {
+    lastId = JSON.parse(lastId);
+  }
+})
+
+var lastId = localStorage.getItem("lastId");
+var solicitations = localStorage.getItem("solicitations");
+
+function save() {
+
+  var formData = {
+    id: ++lastId,
+    name: $("#nomeSolicitanteIn").val(),
+    sector: $("#setorIn").val(),
+    school: $("#escolasIn").val(),
+    date: $("#dataIn").val(),
+    departure: $("#saidaIn").val(),
+    arrival: $("#retornoIn").val(),
+    peopleAmount: $("#numPessoasIn").val(),
+    agenda: $("#programacaoIn").val(),
+    observation: $("#observacaoArea").val(),
+  }
+
+  if (solicitations == null) {
+    localStorage.setItem("solicitations", JSON.stringify([formData]));
+  }else{
+    solicitations = JSON.parse(solicitations);
+    solicitations.push(formData);
+    localStorage.setItem("solicitations", JSON.stringify(solicitations));
+  }
+}
